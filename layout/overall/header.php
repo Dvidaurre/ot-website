44e3e99f8a6ece49f8eccf3488173f862bbfa867
your_usernameDvidaurre <d.vidaurres@gmail.com>
2020-08-09 17:24:09 -0400

    fix downloads section

--- layout/overall/header.php
+++ layout/overall/header.php
@@ -39,61 +39,61 @@
 		<script type="text/javascript" src="layout/javascripts/initialize.js"></script>
 	</head>
 	<body onBeforeUnLoad="SaveMenu();" onUnload="SaveMenu();" onLoad="SetFormFocus()">
-	<div class="top">
+	<div class="">
 		<a href="register.php">
-							<?php
-							$date = 'Aug 15  2020 16:00:00 CET';
-							$exp_date = strtotime($date);
-							$now = time();
+			<?php
+			$date = 'Aug 14  2020 16:00:00 CET';
+			$exp_date = strtotime($date);
+			$now = time();
 
-							if ($now < $exp_date) {
-							?>
-							<script>
-							// Count down milliseconds = server_end - server_now = client_end - client_now
-							var server_end = <?php echo $exp_date; ?> * 1000;
-							var server_now = <?php echo time(); ?> * 1000;
-							var client_now = new Date().getTime();
-							var end = server_end - server_now + client_now; // this is the real end time
+			if ($now < $exp_date) {
+			?>
+			<script>
+			// Count down milliseconds = server_end - server_now = client_end - client_now
+			var server_end = <?php echo $exp_date; ?> * 1000;
+			var server_now = <?php echo time(); ?> * 1000;
+			var client_now = new Date().getTime();
+			var end = server_end - server_now + client_now; // this is the real end time
 
-							var _second = 1000;
-							var _minute = _second * 60;
-							var _hour = _minute * 60;
-							var _day = _hour *24
-							var timer;
+			var _second = 1000;
+			var _minute = _second * 60;
+			var _hour = _minute * 60;
+			var _day = _hour *24
+			var timer;
 
-							function showRemaining()
-							{
-								var now = new Date();
-								var distance = end - now;
-								if (distance < 0 ) {
-								   clearInterval( timer );
-								   document.getElementById('countdown').innerHTML = 'EXPIRED!';
+			function showRemaining()
+			{
+				var now = new Date();
+				var distance = end - now;
+				if (distance < 0 ) {
+					clearInterval( timer );
+					document.getElementById('countdown').innerHTML = 'EXPIRED!';
 
-								   return;
-								}
-								var days = Math.floor(distance / _day);
-								var hours = Math.floor( (distance % _day ) / _hour );
-								var minutes = Math.floor( (distance % _hour) / _minute );
-								var seconds = Math.floor( (distance % _minute) / _second );
+					return;
+				}
+				var days = Math.floor(distance / _day);
+				var hours = Math.floor( (distance % _day ) / _hour );
+				var minutes = Math.floor( (distance % _hour) / _minute );
+				var seconds = Math.floor( (distance % _minute) / _second );
 
-								var countdown = document.getElementById('countdown');
-								countdown.innerHTML = '';
-								if (days) {
-									countdown.innerHTML += ' <span style="color:black;">' + days + '</span> DAYS ';
-								}
-								countdown.innerHTML += ' <span style="color:white;">' + hours+ '</span> HOURS';
-								countdown.innerHTML += ' <span style="color:white;">' + minutes+ '</span> MINUTES';
-								countdown.innerHTML += ' <span style="color:white;">' + seconds+ '</span> SECONDS';
-							}
+				var countdown = document.getElementById('countdown');
+				countdown.innerHTML = '';
+				if (days) {
+					countdown.innerHTML += ' <span style="color:black;">' + days + '</span> DAYS ';
+				}
+				countdown.innerHTML += ' <span style="color:white;">' + hours+ '</span> HOURS';
+				countdown.innerHTML += ' <span style="color:white;">' + minutes+ '</span> MINUTES';
+				countdown.innerHTML += ' <span style="color:white;">' + seconds+ '</span> SECONDS';
+			}
 
-							timer = setInterval(showRemaining, 1000);
-							</script>
-							Perfect Tibia will start in: <span style="color: white;" id="countdown">loading...</span>
-							<?php
-							} else {
-								echo 'Perfect Tibia e 8.0 Will Start In: <span style="color: black;">SERVER STARTED!</span>';
-							}
-							?>
+			timer = setInterval(showRemaining, 1000);
+			</script>
+			Perfect Tibia will start in: <span style="color: white;" id="countdown">loading...</span>
+			<?php
+			} else {
+				echo 'Perfect Tibia e 8.0 Will Start In: <span style="color: black;">SERVER STARTED!</span>';
+			}
+			?>
 		</a>
 	</div>
 		<a name="top" ></a>
