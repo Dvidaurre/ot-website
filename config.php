1505f0199188cabb18d99054c58e5f9e8d86c7cd
Rafael <rafael@vidaurre.io>
2020-08-12 20:38:59 -0400

    revert townId

--- config.php
+++ config.php
@@ -459,7 +459,7 @@
 			'novocation' => array( // Vocation id 0 (No vocation) special settings
 				'level' => 1,
 				'forceTown' => true,
-				'townId' => 1
+				'townId' => 6
 			),
 			'skills' => array( // See $config['vocations'] for proper vocation names of these IDs
 				// No vocation
