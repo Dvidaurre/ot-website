024fdc13b7b5a4c140f6d364c4088d92191b6974
Rafael <rafael@vidaurre.io>
2020-08-12 20:56:49 -0400

    fix fucked up newsticker

--- layout/javascripts/newsticker.js
+++ layout/javascripts/newsticker.js
@@ -7,35 +7,34 @@
 state = new Array("0", "0", "0", "0", "0");
 
 function TickerAction(id) {
-  var line = id.substr(12, 1);
-  if(state[line] == "0") {
-    state[line] = "1";
-    OpenNews(id);
-  }
-  else {
-    state[line] = "0";
-    CloseNews(id);
-  }
+	var line = id.substr(12, 1);
+	if (state[line] == "0") {
+		state[line] = "1";
+		OpenNews(id);
+	} else {
+		state[line] = "0";
+		CloseNews(id);
+	}
 }
 
-function OpenNews(id)
-{
-  var div = document.getElementById(id)
-  var idShort = id.concat("-ShortText");
-  var idMore = id.concat("-FullText");
-  var idButton = id.concat("-Button");
-  document.getElementById(idShort).style.display = "none";
-  document.getElementById(idMore).style.display = "block";
-  document.getElementById(idButton).style.backgroundImage = "url('" + IMAGES + "/global/general/minus.gif')";
+function OpenNews(id) {
+	var div = document.getElementById(id);
+	var idShort = id.concat("-ShortText");
+	var idMore = id.concat("-FullText");
+	var idButton = id.concat("-Button");
+	document.getElementById(idShort).style.display = "none";
+	document.getElementById(idMore).style.display = "block";
+	document.getElementById(idButton).style.backgroundImage =
+		"url('" + IMAGES + "/global/general/minus.gif')";
 }
 
-function CloseNews(id)
-{
-  var div = document.getElementById(id)
-  var idShort = id.concat("-ShortText");
-  var idMore = id.concat("-FullText");
-  var idButton = id.concat("-Button");
-  document.getElementById(idShort).style.display = "block";
-  document.getElementById(idMore).style.display = "none";
-  document.getElementById(idButton).style.backgroundImage = "url('" + IMAGES + "/global/general/plus.gif')";
+function CloseNews(id) {
+	var div = document.getElementById(id);
+	var idShort = id.concat("-ShortText");
+	var idMore = id.concat("-FullText");
+	var idButton = id.concat("-Button");
+	document.getElementById(idShort).style.display = "block";
+	document.getElementById(idMore).style.display = "none";
+	document.getElementById(idButton).style.backgroundImage =
+		"url('" + IMAGES + "/global/general/plus.gif')";
 }
